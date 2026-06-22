435bc8541b34aff6ce468ebb4910cd18ffd1e7e7
pedrodev2026 <pedrodev2026@gmail.com>
2026-06-21 21:22:15 -0300

    edit SDA/SCK GPIO

--- src/drivers/devices/esp32C3042OLED.h
+++ src/drivers/devices/esp32C3042OLED.h
@@ -2,11 +2,11 @@
 #define _ESP32_C3_042_OLED_H
 
 #ifndef SDA_PIN
-#define SDA_PIN 5
+#define SDA_PIN 33
 #endif
 
 #ifndef SCL_PIN
-#define SCL_PIN 6
+#define SCL_PIN 32
 #endif
 
 #define OLED_042_DISPLAY
